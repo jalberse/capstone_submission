c0ac55fe618f0dfcb65399925747fac3dbd1745c
jalberse <alberse.john@gmail.com>
2019-12-21 15:34:42 -0600

    Basic quick emoji and text response

--- quick_response.py
+++ quick_response.py
@@ -1,4 +1,4 @@
-
+import json
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # VADER sentiment analysis is licensed under the MIT license
@@ -8,20 +8,37 @@ class TextAnalyzer:
     def __init__(self):
         self.vader = SentimentIntensityAnalyzer()
         # TODO fill in and load config files mapping sentiment to responses
+        with open('emoji_response.json', 'r') as f:
+            self.emoji_responses = json.load(f)
+        with open('text_response.json', 'r') as f:
+            self.text_responses = json.load(f)
 
     def get_emoji_response(self, text):
         snt = self.vader.polarity_scores(text)
-        print(snt['compound'])
-        return snt
+        sentiment = self.get_sentiment_class(snt)
+        return self.emoji_responses[sentiment]
 
     def get_text_response(self, text):
         snt = self.vader.polarity_scores(text)
-        print(snt['compound'])
-        return snt
+        sentiment = self.get_sentiment_class(snt)
+        return self.text_responses[sentiment]
+
+    def get_sentiment_class(self, snt):
+        if snt['compound'] >= 0.5:
+            sentiment = 'very_positive'
+        elif snt['compound'] >= 0.1:
+            sentiment = 'positive'
+        elif snt['compound'] >= 0:
+            sentiment = 'neutral'
+        elif snt['compound'] >= -0.5:
+            sentiment = 'negative'
+        else:
+            sentiment = 'very_negative'
+        return sentiment
 
 if __name__ == '__main__':
     analyzer = TextAnalyzer()
 
     tmp = 'I did not like this but my cousin did.'
 
-    print(analyzer.get_emoji_response(tmp))
+    print(analyzer.get_text_response(tmp))
