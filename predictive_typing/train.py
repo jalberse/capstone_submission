5a838f42aa1581f4669c1328d05da658084eb1a9
John Alberse <alberse.john@gmail.com>
2020-03-26 17:19:08 -0500

    output verification example in train.py need to integrate into API

--- predictive_typing/train.py
+++ predictive_typing/train.py
@@ -1,5 +1,7 @@
 from predictive_typing import text_predictor
 import matplotlib.pyplot as plt
+import nltk
+import enchant
 
 # TODO Hyperparameter tuning
 # TODO example on how to verify output in
@@ -41,8 +43,39 @@ if __name__ == '__main__':
     test_set = [
         "Sorry to hear th",
         "hi! How can we help you? This is a big issue for us and",
+        "Maybe hire collea",
+        "Hi Thomas, this is correct but this he",
+        "What seems to be the probl",
+        "Hi! I appreciate the fix, can y",
     ]
 
+    d = enchant.Dict("en_US")
+
+    # Example of how to do output verification
     for test in test_set:
+        preds = tp.predict_completions(test,n=5)
+        results = []
+        for pred in preds:
+            full_text = test + pred
+            last_word = full_text.split()[-1]
+            if pred[0] == ' ':
+                # If we are predicting the next word,
+                # we don't want to be able to just go
+                # "y consent" instead of "you" or "yet"
+                # - the second the last word must also be valid
+                second_last_word = full_text.split()[-2]
+                if d.check(last_word) and d.check(second_last_word):
+                    results.append(pred)
+            elif d.check(last_word):
+                results.append(pred)
+
+        print()
+        print()
         print(test)
-        print(tp.predict_completions(test,n=5))
+        print('raw predictions:')
+        print(preds)
+        print('after output verification:')
+        print(results)
+        print('That is, the sentences:')
+        for result in results:
+            print(test + result)
