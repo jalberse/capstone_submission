9cd6be561323068797545a4a047e7b577099d25d
jalberse <alberse.john@gmail.com>
2020-04-08 18:23:53 -0500

    move run into if name main

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ from request_queue import CommentQueue
 from emoji_response_api import EmojiResponseAPI
 from text_response_api import TextResponseAPI
 
-# Initiates flask app and creates api object for this app
-app = Flask(__name__)
-CORS(app)
-api = Api(app)
-
-# app = Flask(__name__)
-
-# Gives app appropriate context
-with app.app_context():
-    analyzer = TextAnalyzer()
-    commentQueue = CommentQueue()
-
-# Adds CommentAPI to flask app under path /textresponse/commentID
-api.add_resource(TextResponseAPI, "/textresponse/<string:commentID>")
-
-# Adds CommentAPI to flask app under path /emojiresponse/commentID
-api.add_resource(EmojiResponseAPI, "/emojiresponse/<string:commentID>")
-
-# Sets debug mode to true for development purposes
-app.run(debug=True)
+if __name__ == '__main__':
+    
+    # Initiates flask app and creates api object for this app
+    app = Flask(__name__)
+    CORS(app)
+    api = Api(app)
+    
+    # app = Flask(__name__)
+    
+    # Gives app appropriate context
+    with app.app_context():
+        analyzer = TextAnalyzer()
+        commentQueue = CommentQueue()
+    
+    # Adds CommentAPI to flask app under path /textresponse/commentID
+    api.add_resource(TextResponseAPI, "/textresponse/<string:commentID>")
+    
+    # Adds CommentAPI to flask app under path /emojiresponse/commentID
+    api.add_resource(EmojiResponseAPI, "/emojiresponse/<string:commentID>")
+    
+    # Sets debug mode to true for development purposes
+    app.run(debug=True)
+    
