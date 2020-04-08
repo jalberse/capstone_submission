353c17f9d05193c88a06bcf61407da1e9ff0e3d5
jalberse <alberse.john@gmail.com>
2020-04-08 18:46:39 -0500

    change host

--- main.py
+++ main.py
@@ -9,25 +9,24 @@ from emoji_response_api import EmojiResponseAPI
 from text_response_api import TextResponseAPI
 
 if __name__ == '__main__':
-    
+
     # Initiates flask app and creates api object for this app
     app = Flask(__name__)
     CORS(app)
     api = Api(app)
-    
+
     # app = Flask(__name__)
-    
+
     # Gives app appropriate context
     with app.app_context():
         analyzer = TextAnalyzer()
         commentQueue = CommentQueue()
-    
+
     # Adds CommentAPI to flask app under path /textresponse/commentID
     api.add_resource(TextResponseAPI, "/textresponse/<string:commentID>")
-    
+
     # Adds CommentAPI to flask app under path /emojiresponse/commentID
     api.add_resource(EmojiResponseAPI, "/emojiresponse/<string:commentID>")
-    
+
     # Sets debug mode to true for development purposes
-    app.run(debug=True)
-    
+    app.run(debug=True,host='0.0.0.0',port=8080)
