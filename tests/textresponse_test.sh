7ac8be867735fe5b21d97fcfda839ec8a037d15e
jalberse <alberse.john@gmail.com>
2020-04-09 17:16:14 -0500

    improve remote test file

--- tests/textresponse_test.sh
+++ tests/textresponse_test.sh
@@ -2,3 +2,9 @@ curl --request POST \
   --url https://capstone-api-273617.uc.r.appspot.com/textresponse/new \
   --header 'content-type: multipart/form-data; boundary=---011000010111000001101001' \
   --form 'text=Do not eat here. Their food is stupid good. You'\''ll go broke due to not wanting to ever eat anywhere else. '
+
+
+curl --request POST \
+  --url https://capstone-api-273617.uc.r.appspot.com/emojiresponse/new \
+  --header 'content-type: multipart/form-data; boundary=---011000010111000001101001' \
+  --form 'text=Do not eat here. Their food is stupid good. You'\''ll go broke due to not wanting to ever eat anywhere else. '
